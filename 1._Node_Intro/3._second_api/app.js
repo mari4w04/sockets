4d097101c222d54f3ad93d07ae47a407415403c4
Anders Latif <anders@github.com>
2019-09-12 13:00:59 +0200

    Showed how to server static files

--- 1._Node_Intro/3._second_api/app.js
+++ 1._Node_Intro/3._second_api/app.js
@@ -1,3 +1,29 @@
-// import and instantiate express
+// const app = require("express")();
+const express = require("express");
+const app = express();
 
-// add app.listen() on port 3000 with a start up message
+// allow to serve static files from here
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/../public/index.html");
+});
+
+// query string
+// /about?firstName=______&lastName=_________
+app.get("/about", (req, res) => {
+    res.send(req.query.firstName + " " + req.query.lastName);
+});
+
+// Request parameter / URL parameter
+app.get("/age/:myAge/:height", (req, res) => {
+    res.send(req.params.myAge);
+});
+
+
+const server = app.listen(3000, (error) => {
+    if (error) {
+        console.log("Error running the server ", error);
+    }
+    console.log("The server is running on port ", server.address().port);
+});
