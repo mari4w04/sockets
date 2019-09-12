81c51946eaa9fd355aec94ae2739ffb45323dc0c
Anders Latif <anders@github.com>
2019-09-12 14:35:36 +0200

    Able to send post requests

--- 1._Node_Intro/3._second_api/app.js
+++ 1._Node_Intro/3._second_api/app.js
@@ -2,6 +2,13 @@
 const express = require("express");
 const app = express();
 
+const bodyParser = require("body-parser");
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
 // allow to serve static files from here
 app.use(express.static('public'));
 
@@ -15,11 +22,22 @@ app.get("/about", (req, res) => {
     res.send(req.query.firstName + " " + req.query.lastName);
 });
 
+app.get("/aboutme", (req, res) => {
+    const response = {
+        firstName: "Anders",
+        lastName: "Latif"
+    };
+    res.json(response);
+});
+
 // Request parameter / URL parameter
 app.get("/age/:myAge/:height", (req, res) => {
     res.send(req.params.myAge);
 });
 
+app.post("/myfavoriteanimal", (req, res) => {
+    res.send("Your favorite animal is: " + req.body.favoriteAnimal);
+}); 
 
 const server = app.listen(3000, (error) => {
     if (error) {
